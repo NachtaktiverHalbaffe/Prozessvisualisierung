f9756b2e5439ce1ef5065bc10ee5bd8cc592b157
st157476 <st157476@stud.uni-stuttgart.de>
2021-06-08 15:27:52 +0200

    first execution of carrierdetection

--- carrierdetection/carrierdetection.py
+++ carrierdetection/carrierdetection.py
@@ -6,7 +6,7 @@ Short description: Module for detecting the carrier with ultrasonic sensor
 (C) 2003-2021 IAS, Universitaet Stuttgart
 
 """
-#import Rpi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
 
@@ -14,19 +14,21 @@ class CarrierDetection(object):
 
     def __init__(self):
         self.GPIO_TRIGGER_1 = 23  # GPIO23
-        self.GPIO_TRIGGER_2 = 4  # GPIO4
+        self.GPIO_TRIGGER_2 = 16  # GPIO4
         self.GPIO_ECHO_1 = 24  # GPIO24
-        self.GPIO_ECHO_2 = 17  # GPIO4
-        # GPIO.setmode(GPIO.BOARD)
-        # GPIO.setwarnings(False)
-        # GPIO.setup(self.GPIO_TRIGGER_1, GPIO.OUT)
-        # GPIO.setup(self.GPIO_ECHO_1, GPIO.IN)
-        # GPIO.setup(self.GPIO_TRIGGER_2, GPIO.OUT)
-        # GPIO.setup(self.GPIO_ECHO_2, GPIO.IN)
+        self.GPIO_ECHO_2 = 18  # GPIO4
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.GPIO_TRIGGER_1, GPIO.OUT)
+        GPIO.setup(self.GPIO_ECHO_1, GPIO.IN)
+        GPIO.setup(self.GPIO_TRIGGER_2, GPIO.OUT)
+        GPIO.setup(self.GPIO_ECHO_2, GPIO.IN)
 
     def calibrate(self):
-        distanceEntrance = self._measureEntrance()
-        distanceExit = self._measureExit()
+        #distanceEntrance = self._measureEntrance()
+        #distanceExit = self._measureExit()
+        distanceEntrance= 0
+        distanceExit= 0
         if abs(distanceEntrance - distanceExit) < 2:
             return (distanceEntrance+distanceExit)/2
         else:
@@ -60,41 +62,35 @@ class CarrierDetection(object):
                     isExpected = True
                     break
 
-        # GPIO.cleanup()
+        GPIO.cleanup()
         return isExpected
 
     def _measureEntrance(self):
-        try:
-            #GPIO.output(self.GPIO_TRIGGER_1, True)
-            time.sleep(0.00002)
-            #GPIO.output(self.GPIO_TRIGGER_1, False)
+        GPIO.output(self.GPIO_TRIGGER_1, True)
+        time.sleep(0.00002)
+        GPIO.output(self.GPIO_TRIGGER_1, False)
+        time_start = time.time()
+        time_end = time.time()
+        while GPIO.input(self.GPIO_ECHO_1) == False:
             time_start = time.time()
+        while GPIO.input(self.GPIO_ECHO_1) == True:
             time_end = time.time()
-            # while GPIO.input(self.GPIO_ECHO_1) == False:
-            #     time_start = time.time()
-            # while GPIO.input(self.GPIO_ECHO_1) == True:
-            #     time_end = time.time()
-            # the measured distance is output in cm
-            # distance = (delta_time * schallgeschw.)/ 2
-            return ((time_end - time_start) * 34300) / 2
+        # the measured distance is output in cm
+        # distance = (delta_time * schallgeschw.)/ 2
+        return ((time_end - time_start) * 34300) / 2
 
-        except Exception as e:
-            pass
 
     def _measureExit(self):
-        try:
-            #GPIO.output(self.GPIO_TRIGGER_2, True)
-            time.sleep(0.00002)
-            #GPIO.output(self.GPIO_TRIGGER_2, False)
+        GPIO.output(self.GPIO_TRIGGER_2, True)
+        time.sleep(0.00002)
+        GPIO.output(self.GPIO_TRIGGER_2, False)
+        time_start = time.time()
+        time_end = time.time()
+        while GPIO.input(self.GPIO_ECHO_2) == False:
             time_start = time.time()
-            time_end = time.time()
-            # while GPIO.input(self.GPIO_ECHO_2) == False:
-            #     time_start = time.time()
-            # while GPIO.input(self.GPIO_ECHO_2) == True:
-            #     time_end = time.time()
-            # the measured distance is output in cm
-            # distance = (delta_time * schallgeschw.)/ 2
-            return ((time_end - time_start) * 34300) / 2
+        while GPIO.input(self.GPIO_ECHO_2) == True:
+                time_end = time.time()
+        # the measured distance is output in cm
+        # distance = (delta_time * schallgeschw.)/ 2
+        return ((time_end - time_start) * 34300) / 2
 
-        except Exception as e:
-            pass
