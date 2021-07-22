8316ebbd1c5e17b039e66b33560bcf116fc72a91
NachtaktiverHalbaffe <{e-A17XEv%q7U&>
2021-07-22 14:25:45 +0200

    set right ip

--- api/constants.py
+++ api/constants.py
@@ -12,9 +12,7 @@ import logging
 from carrierdetection.carrierdetection import CarrierDetection
 
 CWP_DIR = os.path.dirname(os.getcwd())
-#IP_MES = "http://129.69.102.129"
-#IP_MES = "http://127.0.0.1"
-IP_MES = "http://192.168.178.30"
+IP_MES = "http://129.69.102.129"
 calibrater = CarrierDetection()
 calibrater.calibrate()
 BASE_LEVEL_HEIGHT = calibrater.baseLevel
